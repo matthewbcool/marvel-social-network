feadf5d2953afeb1f6e8cd7c1fb6a8afc737c051
Matt Cool <matthewbcool@gmail.com>
2019-08-15 12:23:03 -0500

    add middleware for auth route add user model with jwt and password encrypt to user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
-
+const User = require('../../models/User')
 router.post(
   '/',
   [
@@ -14,13 +18,64 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
-    console.log(req.body)
-    res.send('User route')
+
+    const { name, email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] })
+      }
+      //check if user exist
+      // if exist send error
+      // Get user gravatar?
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      })
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      })
+      // encrypt pass using bcrypt
+      const salt = await bcrypt.genSalt(10)
+
+      user.password = await bcrypt.hash(password, salt)
+
+      // return JWT
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err
+          return res.json({ token })
+        }
+      )
+      await user.save()
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
+    }
+
+    res.send('User registered')
   }
 )
 
