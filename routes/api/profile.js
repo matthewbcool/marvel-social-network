e0ec425e3473c28f0c45ad339834a5ca92dc21a5
Matt Cool <matthewbcool@gmail.com>
2019-08-16 12:34:16 -0500

    add profile info post route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
-
+const { check, validationResult } = require('express-validator')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 
@@ -20,6 +20,37 @@ router.get('/me', auth, async (req, res) => {
   } catch (err) {
     console.error(err.message)
     res.status(500).send('server error')
+  }
+})
+
+router.post('/', auth, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  const { name, bio, photo } = req.body
+  const profileFields = {}
+  profileFields.user = req.user.id
+  if (name) profileFields.name = name
+  if (bio) profileFields.bio = bio
+  if (photo) profileFields.photo = photo
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id })
+    if (profile) {
+      await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true }
+      )
+      return res.json(profile)
+    }
+    profile = new Profile(profileFields)
+    await Profile.save()
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
   }
 })
 
