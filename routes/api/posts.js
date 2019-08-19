f31a1965490236f457ec4eb5330ef8b9f534ec7c
Matt Cool <matthewbcool@gmail.com>
2019-08-19 17:22:52 -0500

    add like and unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -93,4 +93,46 @@ router.delete('/:id', auth, async (req, res) => {
   }
 })
 
+// like a post
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' })
+    }
+    post.likes.unshift({ user: req.user.id })
+    await post.save()
+    res.json(post.likes)
+    res.json({ msg: 'Post removed' })
+  } catch (err) {
+    res.status(500).send('Server Error')
+  }
+})
+
+//unlike a post
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (
+      (post.likes.filter(
+        like => like.user.toString() === req.user.id
+      ).length = 0)
+    ) {
+      return res.status(400).json({ msg: 'Post has not been liked' })
+    }
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id)
+    post.likes.splice(removeIndex, 1)
+
+    await post.save()
+    res.json(post.likes)
+    res.json({ msg: 'Post removed' })
+  } catch (err) {
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
