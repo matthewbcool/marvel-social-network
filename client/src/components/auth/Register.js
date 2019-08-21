a6bf8313f8e4ecbf1d1881661d925cc37976857c
Matt Cool <matthewbcool@gmail.com>
2019-08-21 15:40:11 -0500

    add sign up form hooks

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,30 @@
-import React, { Fragment } from 'react'
+import React, { useState, Fragment } from 'react'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const Register = () => {
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    heroId: '',
+    password: '',
+    password2: ''
+  })
+
+  const { email, heroId, password, password2 } = registerData
+  const onChange = event => {
+    setRegisterData({
+      ...registerData,
+      [event.target.name]: event.target.value
+    })
+  }
+  const submitRegister = event => {
+    event.preventDefault()
+    console.log('form submitted', registerData)
+    setRegisterData(registerData)
+    console.log(registerData)
+  }
+
   return (
     <Fragment>
       <form noValidate autoComplete='off'>
@@ -10,6 +33,7 @@ const Register = () => {
           label='Email'
           type='email'
           name='email'
+          onChange={onChange}
           autoComplete='email'
           margin='normal'
           variant='outlined'
@@ -17,19 +41,41 @@ const Register = () => {
         <TextField
           id='outlined-email-input'
           label='Hero Id'
+          onChange={onChange}
           type='number'
-          name='heroid'
+          name='heroId'
           margin='normal'
           variant='outlined'
         />
         <TextField
           id='outlined-password-input'
           label='Password'
+          name='password'
           type='password'
+          onChange={onChange}
           autoComplete='current-password'
           margin='normal'
           variant='outlined'
         />
+        <TextField
+          id='outlined-password-input-2'
+          label='Re-type Password'
+          name='password2'
+          type='password'
+          onChange={onChange}
+          autoComplete='current-password'
+          margin='normal'
+          variant='outlined'
+        />
+        <Button
+          type='submit'
+          onSubmit={submitRegister}
+          onClick={submitRegister}
+          contained
+          variant='contained'
+          color='primary'>
+          Sign-Up
+        </Button>
       </form>
     </Fragment>
   )
